6404df956deae6593b98312d1700b41a6c1a9e99
Roberto Acosta <x.rojo@hotmail.com>
2020-09-24 04:56:27 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,13 +1,13 @@
 def second_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
+      :patriarch => {},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
